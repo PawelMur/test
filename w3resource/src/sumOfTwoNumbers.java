601e26d52c2b925d69a67f62f7363ad34af80152
Paweł <malysdi@poczta.onet.pl>
2019-01-26 12:40:51 +0100

    nowy plik

---
+++ w3resource/src/sumOfTwoNumbers.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+public class sumOfTwoNumbers {
+    public static Integer sum(int i, int j){
+        return i + j;
+    }
+    public static void main (String[]args){
+        System.out.println(sum(5,10));
+    }
+}
